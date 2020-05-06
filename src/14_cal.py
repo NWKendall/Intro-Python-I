6574ccdd269edb70b2f5634df1ce83c1cbea5a29
nicbongo <nicbongo@protonmail.com>
2020-05-05 21:25:02 -0300

    14_cal

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,41 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+
+def cal_func():
+    now = datetime.today()
+    # return current month if no input
+    print("length:", len(sys.argv))
+
+    if len(sys.argv) > 3:
+        print("Please provide only a month (mm) and year (yyyy)")
+    elif len(sys.argv) == 1:
+        print(calendar.TextCalendar().prmonth(
+            now.year, now.month))
+    elif len(sys.argv) == 2:
+        print(calendar.TextCalendar().prmonth(
+            now.year, int(sys.argv[1])))
+    elif sys.argv[1] and sys.argv[2]:
+        print(calendar.TextCalendar().prmonth(
+            int(sys.argv[2]), int(sys.argv[1])))
+    # if 1 input, render month
+    # if 2, month and year
+    # error handling
+    # if no args, return todays month
+
+
+# cal_func(sys.argv[1], sys.argv[2])
+cal_func()
